0256a85e311d7e5d2178f4889d59465e9b215bbe
milower <sirapop04@gmail.com>
2016-02-10 10:08:37 +0700

    Check Regis Finish OwO/

--- app/src/main/java/com/example/maste2seven/weightcheck/MainActivity.java
+++ app/src/main/java/com/example/maste2seven/weightcheck/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.maste2seven.weightcheck;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.IllegalFormatCodePointException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         //requestdatabase
         objMyManage = new MyManage(this);
 
+        //Check Register
+        checkRegister();
+
     }//main method
-}//main class
+
+    private void checkRegister() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,MODE_PRIVATE,null);
+        Cursor objCursor = objSqLiteDatabase.rawQuery("SELECT * FROM userName",null);
+        if (objCursor.getCount() == 0) {
+            Intent objIntent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(objIntent);
+        }
+        objCursor.close();
+    }
+}//main classr
